7b84d01190a3fa66cd83d338d9bf64cd1ae2ecd7
Janamsdn <janamsdn@gmail.com>
2016-02-01 21:51:48 +0000

    LiveNewsVedioAndWeb
    LiveNewsFrom Youtube and Web best Live news

---
+++ app/src/main/java/com/jay/livenews/VideoClickListener.java
@@ -0,0 +1,10 @@
+package com.jay.livenews;
+
+import com.jay.livenews.domain.Video;
+
+/**
+ * Created by franwu on 4/11/2015.
+ */
+public interface VideoClickListener {
+    public void onVideoClicked(Video video);
+}
---
+++ app/src/main/java/com/jay/livenews/domain/ObjectDrawerItem.java
@@ -0,0 +1,16 @@
+package com.jay.livenews.domain;
+
+/**
+ * Created by franwu on 4/14/2015.
+ */
+public class ObjectDrawerItem {
+    public int icon;
+    public String name;
+
+    // Constructor.
+    public ObjectDrawerItem(int icon, String name) {
+
+        this.icon = icon;
+        this.name = name;
+    }
+}
